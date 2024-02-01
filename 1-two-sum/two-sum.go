651da73d5eb84f95ff1a504354e7f2d21af2d3c6
Gralach <62006566+Gralach@users.noreply.github.com>
2024-02-01 22:16:00 +0800

    Time: 6 ms (77.29%) | Memory: 4.2 MB (18.43%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,9 +1,10 @@
 func twoSum(nums []int, target int) []int {
+    myMap := make(map[int]int)
     for index, value := range nums{
-        for index2 := index + 1; index2 < len(nums); index2++ {
-            if value + nums[index2] == target {
-                return []int{index, index2}
-            }
+        if _, exists := myMap[target-value]; exists{
+            return[]int{myMap[target-value], index}
+        } else{
+            myMap[value] = index
         }
     }
     return nil
