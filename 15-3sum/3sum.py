2cc2859565b26ef07633bb410cdb480d6f789988
Gralach <62006566+Gralach@users.noreply.github.com>
2024-09-10 18:54:24 +0800

    Time: 942 ms (25.07%) | Memory: 20.7 MB (72.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -9,23 +9,23 @@ class Solution:
             return self.quickSort(left) + [pivot] + self.quickSort(right)
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        sortednums = self.quickSort(nums)
+        nums.sort()
         i = 0
         arr = []
-        for i in range(len(sortednums) - 2):
-            if i != 0 and sortednums[i-1] == sortednums[i]:
+        for i in range(len(nums) - 2):
+            if i != 0 and nums[i-1] == nums[i]:
                 continue
             j = i + 1
-            k = len(sortednums)-1
+            k = len(nums)-1
             while k > j:
-                if j != i+1 and sortednums[j-1] == sortednums[j]:
+                if j != i+1 and nums[j-1] == nums[j]:
                     j += 1
-                elif sortednums[i] + sortednums[j] + sortednums[k] > 0:
+                elif nums[i] + nums[j] + nums[k] > 0:
                     k -= 1
-                elif sortednums[i] + sortednums[j] + sortednums[k] < 0:
+                elif nums[i] + nums[j] + nums[k] < 0:
                     j += 1
-                elif sortednums[i] + sortednums[j] + sortednums[k] == 0:
-                    arr.append([sortednums[i] , sortednums[j] , sortednums[k]])
+                elif nums[i] + nums[j] + nums[k] == 0:
+                    arr.append([nums[i] , nums[j] , nums[k]])
                     j += 1
         return arr
 
